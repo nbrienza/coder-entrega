27772e2286bc2d3152185c7f97af04a74c36014c
nbrienza <108191651+nbrienza@users.noreply.github.com>
2023-02-22 20:54:38 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+ var nombre = "Nicolas Brienza";
+ var presentacion = <h2>Hola; soy {nombre}</h2>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        {presentacion}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
